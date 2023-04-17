f78fba15a39887bb7fcd5fcde3785cca47b9e412
C520S <71176236+C520S@users.noreply.github.com>
2023-04-17 16:05:22 +0300

    Optimize back-end code

--- models/journeyModel.js
+++ models/journeyModel.js
@@ -3,23 +3,26 @@ const mongoose = require("mongoose");
 
 // Define a schema for journeys
 const journeySchema = new mongoose.Schema({
-  departureTime: { type: String, trim: true },
-  returnTime: { type: String, trim: true },
-  departureStationId: { type: Number, trim: true },
-  departureStationName: { type: String, trim: true },
-  returnStationid: { type: Number, trim: true },
-  returnStationName: { type: String, trim: true },
+  departureTime: { type: String, trim: true, required: true },
+  returnTime: { type: String, trim: true, required: true },
+  departureStationId: { type: Number, trim: true, required: true },
+  departureStationName: { type: String, trim: true, required: true },
+  returnStationid: { type: Number, trim: true, required: true },
+  returnStationName: { type: String, trim: true, required: true },
   coveredDistance: {
     type: Number,
     trim: true,
+    required: true,
     min: [10, "coveredDistance must be above 10m"],
   },
   duration: {
     type: Number,
     trim: true,
+    required: true,
     min: [10, "duration must be above 10s"],
   },
 });
+
 
 // Create a Mongoose model based on the journey schema
 const Journeys = mongoose.model("Journeys", journeySchema);
